66746b6d203e0c4d3d5980e679c448e9c291c7de
tomleblais <leblaistom@gmail.com>
2023-09-25 19:26:35 +0200

    fix: comments added to fetchWeather #2

--- js/weatherRequests.js
+++ js/weatherRequests.js
@@ -1,5 +1,10 @@
 const TOKEN = "626b55c5c509066c2d6e54a48c551379036dcf6f43cd6c06435bc586ded837cc";
 
+/**
+ * Fetch weather info from the specified INSEE code
+ * @param {number} inseeCode INSEE code
+ * @returns {Promise} Promise that resolves with weather info or rejects with an error
+ */
 function fetchWeather(inseeCode){
     return new Promise((resolve, reject) => {
         const regex = /^[a-zA-Z0-9]{5}$/;
