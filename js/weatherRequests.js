48c2b8c24bdc01219a89262f83204c08f9f418db
alexandre <alex.lerosier@gmail.com>
2023-09-25 15:39:00 +0200

    fix: Weather requests #2

--- js/weatherRequests.js
+++ js/weatherRequests.js
@@ -2,22 +2,30 @@ const TOKEN = "626b55c5c509066c2d6e54a48c551379036dcf6f43cd6c06435bc586ded837cc"
 
 function fetchWeather(inseeCode){
     return new Promise((resolve, reject) => {
-        const regex = new RegExp("[a-zA-Z0-9]");
+        const regex = /^[a-zA-Z0-9]{5}$/;
 
-        if (inseeCode.length != 5) {
-            reject(new Error("Invalid insee code"));
-        } else if (regex.test(inseeCode)) {
+        if (!regex.test(inseeCode)) {
             reject(new Error("Invalid code"));
         } else {
             fetchJSON(`https://api.meteo-concept.com/api/forecast/daily?token=${TOKEN}&insee=${inseeCode}`)
             .then(data => {
-                if (data.length > 0) {
-                    resolve(data);
-                } else {
-                    reject(new Error("this INSEE code does not exist"));
-                }
+                resolve(data.forecast[0]);
             })
-            .catch(reject);
+            .catch(error => {
+                if (error instanceof Error) {
+                    reject(new Error("This commune does not exist"));
+                } else {
+                    reject(error);
+                }
+            });
         }
     });
 }
+
+fetchWeather("14999")
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
