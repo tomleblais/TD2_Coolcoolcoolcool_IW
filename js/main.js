7889bf98d9ac6a52410001b8dbb01041e56dd49b
tomleblais <leblaistom@gmail.com>
2023-09-26 14:55:24 +0200

    fix: Select option fixed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 window.addEventListener("load", event => {
+
     const postalCodeInput = document.getElementById("postalCodeInput");
+    const communeSelect = document.getElementById("communeSelect");
+    
     postalCodeInput.addEventListener("change", event => {
         fetchCommune(postalCodeInput.value)
-        .then(data =>{
-            console.log(data);
-        })
-        .catch(console.error)
+            .then(communes => {
+
+                communeSelect.innerHTML = ""; // Remove all children
+                communeSelect.disabled = false;
+
+                communes.forEach(commune => {
+                    const option = printCommuneOption(commune);
+                    communeSelect.appendChild(option);
+                });
+            })
+            .catch(error => {
+                if (error instanceof CommuneNotFoundError) {
+                    communeSelect.innerHTML = "";
+                    communeSelect.disabled = true;
+                }
+            })
     })
 })
